e377df8f0553164d0c3d09640ec5f68c0d7b2fe1
Shlok Singh <theshlok@gmail.com>
2021-10-13 11:24:08 +0530

    program for difference of sets was created

---
+++ differenceOfSets/cmake-build-debug/CMakeFiles/differenceOfSets.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/differenceOfSets.dir/main.c.o"
+  "differenceOfSets"
+  "differenceOfSets.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/differenceOfSets.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
